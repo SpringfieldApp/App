9ae6cf5619446bed5d91d59d1b64078bcbba03be
yash0530 <yashjain0530@gmail.com>
2018-11-05 20:17:05 +0530

    First Commit

---
+++ SpringfieldApp/SpringfieldApp.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SpringfieldApp
+{
+    partial class SpringfieldApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpringfieldApp));
+            this.springFieldHeader = new System.Windows.Forms.Label();
+            this.upload = new System.Windows.Forms.Label();
+            this.browse = new System.Windows.Forms.Button();
+            this.process = new System.Windows.Forms.Button();
+            this.processLabel = new System.Windows.Forms.Label();
+            this.show = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // springFieldHeader
+            // 
+            resources.ApplyResources(this.springFieldHeader, "springFieldHeader");
+            this.springFieldHeader.Name = "springFieldHeader";
+            // 
+            // upload
+            // 
+            resources.ApplyResources(this.upload, "upload");
+            this.upload.Name = "upload";
+            // 
+            // browse
+            // 
+            resources.ApplyResources(this.browse, "browse");
+            this.browse.Name = "browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // process
+            // 
+            resources.ApplyResources(this.process, "process");
+            this.process.Name = "process";
+            this.process.UseVisualStyleBackColor = true;
+            this.process.Click += new System.EventHandler(this.process_Click);
+            // 
+            // processLabel
+            // 
+            resources.ApplyResources(this.processLabel, "processLabel");
+            this.processLabel.Name = "processLabel";
+            // 
+            // show
+            // 
+            resources.ApplyResources(this.show, "show");
+            this.show.Name = "show";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // SpringfieldApp
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.processLabel);
+            this.Controls.Add(this.process);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.springFieldHeader);
+            this.Name = "SpringfieldApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label header;
+        private System.Windows.Forms.Label uploadLabel;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button extractButton;
+        private System.Windows.Forms.Label processedLabel;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Label springFieldHeader;
+        private System.Windows.Forms.Label upload;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button process;
+        private System.Windows.Forms.Label processLabel;
+        private System.Windows.Forms.Button show;
+    }
+}
+
---
+++ SpringfieldApp/SpringfieldApp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpringfieldApp
+{
+    public partial class SpringfieldApp : Form
+    {
+        OpenFileDialog oft = new OpenFileDialog();
+        string pathPDF, fileNamePDF;
+        bool processed = false;
+
+        // pathPDF is the the LOCATION of the pdf file user selected
+        // fileNamePDF is the NAME of the pdf file user selected
+
+        public SpringfieldApp()
+        {
+            InitializeComponent();
+            show.Enabled = false;
+            process.Enabled = false;
+        }
+        
+        // clickListener for BROWSE Button
+        private void browse_Click(object sender, EventArgs e)
+        {
+            oft.Filter = "PDF(*.pdf) | *.pdf";
+            if (oft.ShowDialog() == DialogResult.OK)
+            {
+                pathPDF = oft.FileName;
+                fileNamePDF = oft.SafeFileName;
+                browse.Text = fileNamePDF;
+                process.Enabled = true;
+            }
+        }
+        
+        // clickListener for PROCESS Button
+        private void process_Click(object sender, EventArgs e)
+        {
+            // call the python script on the pdf file
+            // path of that pdf is pathPDF
+
+            // once the processing is done
+            // show.Enabled = true;
+            // processed = true;
+
+            // and save the processed file as csv someWhere
+            // so that it can be given to user when he clicks SHOW button
+        }
+
+        // clickListener for SHOW Button
+        private void show_Click(object sender, EventArgs e)
+        {
+            if (processed)
+            {
+                // once the processing is done this button should get activated
+
+                // open the processed csv file
+            }
+        }
+    }
+}
